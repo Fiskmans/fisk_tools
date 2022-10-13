faec9ad450723d3987ff14dea4c0613fcb99a9f3
Fiskmans - Work <mans@zingo.org>
2022-10-13 13:17:49 +0200

    Added dummy cpp for cmake to be able to determine language

---
+++ tools/Concepts.cpp
@@ -0,0 +1,4 @@
+#include "tools/Concepts.h"
+
+// this file likely does not need to exist, but for now it's the only cpp in the whole project
+// without it cmake is unable to detirmine the linker language for the project
