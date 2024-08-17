f000a723ed197fbdb71ca3d0e206e1245bba22ac
Fiskmans - GLaDOS <mans@zingo.org>
2024-08-17 23:40:23 +0200

    Squashed commit of the following:
    commit d08c30e70fbf0ca81c2ac49bf8fcc27c9554f001
    Author: Fiskmans - GLaDOS <mans@zingo.org>
    Date:   Sat Aug 17 22:39:20 2024 +0200
    
        include arpa/inet.h copy more of win imp
    
    commit 872d81c13858bf192341839b8fd271978342bb54
    Author: Fiskmans - GLaDOS <mans@zingo.org>
    Date:   Sat Aug 17 22:37:31 2024 +0200
    
        include cstring
    
    commit 68cefa7739a858a76e89a40ccab3d3928af07718
    Author: Fiskmans - GLaDOS <mans@zingo.org>
    Date:   Sat Aug 17 22:36:47 2024 +0200
    
        include netinet/in.h
    
    commit 48b5329963884db049951a61fc6e9e5724e56c18
    Author: Fiskmans - GLaDOS <mans@zingo.org>
    Date:   Sat Aug 17 22:35:18 2024 +0200
    
        include sys/socket.h
    
    commit 37f43024a08d3bd273a29fa7c256ed858bd73fb1
    Author: Fiskmans - GLaDOS <mans@zingo.org>
    Date:   Sat Aug 17 22:33:30 2024 +0200
    
        copy windows imp

--- tools/net/LinuxSocket.cpp
+++ tools/net/LinuxSocket.cpp
@@ -5,7 +5,11 @@
 
 #include "tools/net/Socket.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 
 namespace fisk::tools
 {
@@ -15,9 +19,49 @@ namespace fisk::tools
 			::close(myValue);
 	}
 
+	void* GetAddressStart(sockaddr* aSockaddr)
+	{
+		if (aSockaddr->sa_family == AF_INET)
+			return &(reinterpret_cast<sockaddr_in*>(aSockaddr)->sin_addr);
+
+		return &(reinterpret_cast<sockaddr_in6*>(aSockaddr)->sin6_addr);
+	}
+
+	uint16_t GetPort(sockaddr* aSockaddr)
+	{
+		if (aSockaddr->sa_family == AF_INET)
+			return ntohs(reinterpret_cast<sockaddr_in*>(aSockaddr)->sin_port);
+
+		return ntohs(reinterpret_cast<sockaddr_in6*>(aSockaddr)->sin6_port);
+	}
+
 	std::string Socket::GetAddress() const
 	{
-		return "<not yet implemented>";
+		if (myValue == -1)
+			return "<invalid>";
+
+		sockaddr_in6 addr;
+
+		memset(&addr, 0, sizeof(addr));
+
+		socklen_t length = sizeof(addr);
+
+		if (::getpeername(myValue, reinterpret_cast<sockaddr*>(&addr), &length) == -1)
+			return "<error>";
+
+		char s[INET6_ADDRSTRLEN];
+
+		sockaddr* base = reinterpret_cast<sockaddr*>(&addr);
+
+		char stringBuffer[INET6_ADDRSTRLEN];
+		const char* formattedString = inet_ntop(base->sa_family, GetAddressStart(base), stringBuffer, sizeof(stringBuffer));
+
+		if (!formattedString)
+			return "<formatting failed>";
+
+		std::string out = formattedString;
+
+		return out + ":" + std::to_string(GetPort(base));
 	}
 
 } // namespace fisk::tools
